dd21070f32ed97d2a3744917b0b0d2107a21df65
kevmorrin <x00084036@ittd.ie>
2013-12-12 22:09:00 +0000

    Added unit test project

--- TestBusinessReviewApp/Properties/AssemblyInfo.cs
+++ TestBusinessReviewApp/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4914a05b-96d1-4851-b9b4-09812ca6b478")]
+[assembly: Guid("28bb6f96-884b-4d70-af13-0d3821672ad7")]
 
 // Version information for an assembly consists of the following four values:
 //
